0a12f7bc24cbb79856d8ab4b1de01a2a4cb14fe5
Neil Amrami <neilamrami@Neils-MacBook-Pro.local>
2020-09-24 16:57:24 -0700

    TestStash file modified

--- src/com/stash/TestStash.java
+++ src/com/stash/TestStash.java
@@ -1,5 +1,10 @@
 package com.stash;
 
 public class TestStash {
+	public static void main(String[] args) {
+		
+		System.out.println("Modification of our code GitStash Test");
+		System.out.println("Hello World!");
+	}
 
 }
